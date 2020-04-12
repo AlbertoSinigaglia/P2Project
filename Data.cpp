6388219bde52c25cdef5861b20c625cb154767de
Alberto Sinigaglia <sinigagliaalberto@gmail.com>
2020-04-12 23:17:42 +0200

    Review di Data
    Separazione tra .h e .cpp
    proposto semplificazioni di due metodi
    correzzione errori di tipo
    aggiunta metodi addRemove(anni/mesi) per completezza dell'interfaccia
    implemetazione operatore di input output

--- Data.cpp
+++ Data.cpp
@@ -1,70 +1,33 @@
 #include<vector>
+#include "Data.h"
 
-// .H
 
-
-class Data {
-    friend std::istream &operator>>(std::istream &, Data &);
-
-private:
-
-    static const int giorni_al_mese[12];    //gennaio -> 0  , dicembre -> 11
-    static int giorniDelMese(int mese_, int anno_ = 1);
-
-    unsigned int giorno, mese;    // data non esiste (mai) corrisponde ai tre campi = 0
-    int anno;
-
-public:
-
-    static Data oggi();
-    static Data mai();          //corrisponde a nessuna Data possibile
-    //static Data forever();
-    void setGiorno(unsigned int g) { giorno = g; }
-    void setMese(unsigned int m) { mese = m; }
-    void setAnno(int a) { anno = a; }
-    unsigned int getGiorno() { return giorno; }
-    unsigned int getMese() { return mese; }
-    int getAnno() { return anno; }
-    explicit Data(int a, unsigned int m, unsigned int g);
-    Data(int num_giorni = 0);     //dalla data base (0,1,1)
-    operator int();                          // differenza giorni dalla base (0,1,1)
-    bool operator==(const Data &d1) const;
-    bool operator<(const Data &) const;        //assunzione: qualsiasi data è maggiore di una data che non esiste
-    bool operator>(const Data &) const;
-    void addRemoveGiorni(int g);    // accetta anche valori negativi => toglie giorni
-    int differenzaGiorni(const Data &) const;
-    int differenzaMesi(const Data &) const;
-    int differenzaAnni(const Data &) const;
-    Data operator-(
-            const Data &d);   // restituisce un valore Data approssimato di quella che è la distanza temporale assoluta tra le due date
-    //  es. compleanno1 dista 4 anni 3 mesi e 10 giorni da capodanno
-
-};
 
 
 const int Data::giorni_al_mese[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-
-
-//   .CPP
-
-
-int Data::giorniDelMese(int mese_, int anno_) {
+int Data::giorniDelMese(unsigned int mese_, int anno_) {
     return (((mese_ == 1) && (anno_ % 4 == 0)) ? 29 : Data::giorni_al_mese[mese_ - 1]);
 }
 
 Data Data::oggi() {
-    return Data(/*funzione di sistema?*/6, 4, 2020);
+    time_t now = time(nullptr);
+    tm *lt = localtime(&now);
+    return Data(lt->tm_mday, lt->tm_mon, lt->tm_year);
 }
-
-Data Data::mai() {
-    return Data(0, 0, 0);     // data inesistente
-}
-
 
 Data::Data(int a, unsigned int m, unsigned int g) : anno(a), mese(m), giorno(g) {
-    if (mese < 1 || giorno < 1 || mese > 12 || giorno > giorniDelMese(mese, anno))
-        a = m = g = 0;
+
+    if (mese < 1  || mese > 12 )
+        throw std::invalid_argument("Mese non valido");
+    else if(giorno > giorniDelMese(mese, anno) || giorno < 1)
+        throw std::invalid_argument("Giorno non valido");
+    /*
+    if (mese < 1 || giorno < 1 || mese > 12 || giorno > giorniDelMese(mese, anno)){
+        anno = 0;
+        giorno = mese = 1;
+    }
+     */
 }
 
 Data::Data(int num_giorni) {
@@ -74,7 +37,7 @@ Data::Data(int num_giorni) {
     if (num_giorni > 366) {
         num_giorni -= 366;
         a++;
-        a = num_giorni / 365;
+        a += num_giorni / 365;
         num_giorni = num_giorni % 365;
     }
     m = 1;
@@ -86,7 +49,6 @@ Data::Data(int num_giorni) {
     mese = m;
     giorno = num_giorni;
 }
-
 
 bool Data::operator==(const Data &d) const {
     if ((this == &d) || (giorno == 0 && d.giorno == 0)) return true;
@@ -109,7 +71,87 @@ bool Data::operator>(const Data &d) const {
     return !(operator==(d) || operator<(d));
 }
 
+int Data::differenzaGiorni(const Data &d) const {
+    return static_cast<int>(*this) - static_cast<int>(d);
+    /*
+    const Data *max, *min;
+    if (operator>(d)) {
+        max = this;
+        min = &d;
+    }
+    else {
+        max = &d;
+        min = this;
+    }
+    int count_giorni = 0;
+
+    count_giorni = max->giorno + giorniDelMese(min->mese, min->anno) - min->giorno;
+    for (int i = 1; i < max->mese; i++) count_giorni += giorniDelMese(i, max->anno);
+    for (int i = min->mese + 1; i < 13; i++) count_giorni += giorniDelMese(i, min->anno);
+
+    count_giorni += 365 * (max->anno - min->anno - 1) + (max->anno - min->anno + min->anno % 4) / 4 +
+                    ((min->anno % 4 == 0) ? 1 : 0);
+
+    if (max == this) return count_giorni;
+    else return -count_giorni;
+     */
+}
+
+int Data::differenzaMesi(const Data &d) const {
+    int count_mesi = 0;
+    const Data *max, *min;
+    if (operator>(d)) {
+        max = this;
+        min = &d;
+    }
+    else {
+        max = &d;
+        min = this;
+    }
+
+    count_mesi += static_cast<int>(max->mese) + 12 - static_cast<int>(min->mese);
+    count_mesi += 12 * (max->anno - min->anno - 1);
+
+    return count_mesi * (max == this ? 1 : -1);
+}
+
+int Data::differenzaAnni(const Data &d) const {
+    return anno - d.anno;
+}
+
+Data Data::operator-(const Data &d) const {
+    int g, m, a;
+    int x;
+    const Data *max, *min;
+    if (operator>(d)) {
+        max = this;
+        min = &d;
+    }
+    else {
+        max = &d;
+        min = this;
+    }
+
+    x = max->differenzaGiorni(*min);
+    a = x / 365;
+    m = (x % 365) / 30;
+    g = x % 30;
+    return Data(a, m, g);
+}
+
+Data::operator int() const {
+    return differenzaGiorni(Data(0, 1, 1));
+}
+
 void Data::addRemoveGiorni(int g) {
+    int tot_giorni = static_cast<int>(*this);
+    tot_giorni+=g;
+    Data data(tot_giorni);
+    giorno = data.giorno;
+    mese = data.mese;
+    anno = data.anno;
+
+    /*
     if (giorno != 0)
         while (g != 0) {
             if (g < 0) {
@@ -136,80 +178,81 @@ void Data::addRemoveGiorni(int g) {
                 }
                 else mese++;
             }
-        }
+        }*/
 }
 
-int Data::differenzaGiorni(const Data &d) const {
-    const Data *max, *min;
-    if (operator>(d)) {
-        max = this;
-        min = &d;
-    }
-    else {
-        max = &d;
-        min = this;
-    }
-    int count_giorni = 0;
-
-    count_giorni = max->giorno + giorniDelMese(min->mese, min->anno) - min->giorno;
-    for (int i = 1; i < max->mese; i++) count_giorni += giorniDelMese(i, max->anno);
-    for (int i = min->mese + 1; i < 13; i++) count_giorni += giorniDelMese(i, min->anno);
-
-    count_giorni += 365 * (max->anno - min->anno - 1) + (max->anno - min->anno + min->anno % 4) / 4 +
-                    ((min->anno % 4 == 0) ? 1 : 0);
-
-    if (max == this) return count_giorni;
-    else return -count_giorni;
+void Data::addRemoveMesi(int m) {
+    int a_ = m / 12;
+    int m_ = m % 12;
+    mese += m_;
+    anno += a_;
 }
 
-int Data::differenzaMesi(const Data &d) const {
-    int count_mesi;
-    const Data *max, *min;
-    if (operator>(d)) {
-        max = this;
-        min = &d;
-    }
-    else {
-        max = &d;
-        min = this;
-    }
-
-    count_mesi += max->mese + 12 - min->mese;
-    count_mesi += 12 * (max->anno - min->anno - 1);
-
-    if (max == this) return count_mesi;
-    else return -count_mesi;
+void Data::addRemoveAnni(int a) {
+    anno += a;
 }
 
-int Data::differenzaAnni(const Data &d) const {
-    return anno - d.anno;
+void Data::setGiorno(unsigned int g) {
+    if( g > 0 && g<= giorniDelMese(mese, anno) )
+        giorno = g;
+    else
+        throw std::invalid_argument("Giorno non valido");
 }
 
+void Data::setMese(unsigned int m) {
+    if( m > 0 && m <= 12)
+        mese = m;
+    else
+        throw std::invalid_argument("Mese non valido");
+}
 
-Data Data::operator-(const Data &d) {
-    int g, m, a;
-    int x;
-    const Data *max, *min;
-    if (operator>(d)) {
-        max = this;
-        min = &d;
+void Data::setAnno(int a) {
+    anno = a;
+}
+
+unsigned int Data::getGiorno() const {
+    return giorno;
+}
+
+unsigned int Data::getMese() const {
+    return mese;
+}
+
+int Data::getAnno() const {
+    return anno;
+}
+
+std::istream &operator>>(std::istream & input, Data &data) {
+    unsigned int d, m;
+    int y;
+    std::string s;
+
+    input >> s;
+    auto pos_f = s.find('/');
+    if(pos_f == std::string::npos)
+        throw std::invalid_argument("L'input fornito non è nel formato GG/MM/(-)YYYY");
+    std::istringstream (s.substr(0, pos_f)) >> d;
+    s = s.substr(pos_f+1);
+
+    auto pos_s = s.find('/');
+    if(pos_s == std::string::npos)
+        throw std::invalid_argument("L'input fornito non è nel formato GG/MM/(-)YYYY");
+    std::istringstream (s.substr(0, pos_s)) >> m;
+    s = s.substr(pos_s+1);
+
+    std::istringstream (s) >> y;
+
+    try {
+        data = Data(y, m, d);
+    } catch (const std::invalid_argument&){
+        throw std::invalid_argument("L'input fornito non è nel formato GG/MM/(-)YYYY");
     }
-    else {
-        max = &d;
-        min = this;
-    }
-
-    x = max->differenzaGiorni(*min);
-    a = x / 365;
-    m = (x % 365) / 30;
-    g = x % 30;
-    return Data(a, m, g);
+    return input;
 }
 
-Data::operator int() {
-    return differenzaGiorni(Data(0, 1, 1));
+std::ostream &operator<<(std::ostream &os, const Data &data) {
+    return os << data.giorno << "/" << data.mese << "/" << data.anno;
 }
-
 
 /*
 
---
+++ Data.h
@@ -0,0 +1,80 @@
+//
+// Created by Alberto Sinigaglia on 12/04/2020.
+//
+
+#ifndef CSVPARSER_DATA_H
+#define CSVPARSER_DATA_H
+
+#include <istream>
+#include <iostream>
+#include <string>
+#include <sstream>
+class Data {
+    friend std::istream &operator>>(std::istream &, Data &);
+    friend std::ostream &operator<<(std::ostream &,const Data &);
+
+private:
+
+    static const int giorni_al_mese[12];    //gennaio -> 0  , dicembre -> 11
+    static int giorniDelMese(unsigned int mese_, int anno_ = 1);
+    unsigned int giorno, mese;    // data non esiste (mai) corrisponde ai tre campi = 0
+    int anno;
+
+public:
+    /*
+     * Costruttori
+     */
+    explicit Data(int num_giorni = 0);     //dalla data base (0,1,1)
+    explicit Data(int a, unsigned int m, unsigned int g);
+
+    /*
+     * Setter
+     */
+    void setGiorno(unsigned int g);
+    void setMese(unsigned int m);
+    void setAnno(int a);
+
+    /*
+     * Getter
+     */
+    inline unsigned int getGiorno() const;
+    inline unsigned int getMese() const;
+    inline int getAnno() const;
+
+    /*
+     * Setter Helpers
+     */
+    void addRemoveGiorni(int g);    // accetta anche valori negativi => toglie giorni
+    void addRemoveMesi(int m);
+    void addRemoveAnni(int a);
+
+    /*
+     * Getter Helper
+     */
+    int differenzaGiorni(const Data &) const;
+    int differenzaMesi(const Data &) const;
+    int differenzaAnni(const Data &) const;
+
+    /*
+     * Operatori
+     */
+    bool operator==(const Data &d1) const;
+    bool operator<(const Data &) const;        //assunzione: qualsiasi data è maggiore di una data che non esiste
+    bool operator>(const Data &) const;
+    Data operator-(const Data &d) const;   // restituisce un valore Data approssimato di quella che è la distanza temporale assoluta tra le due date
+
+
+    /*
+     * Cast
+     */
+    explicit operator int() const;                          // differenza giorni dalla base (0,1,1)
+
+
+    /*
+     * Metodi statici
+     */
+    static Data oggi();
+
+};
+
+#endif //CSVPARSER_DATA_H
